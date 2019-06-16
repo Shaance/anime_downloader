6a3f8ea70ff6dc5994cce21397f3965b71ba4c47
Shaance <christophe.ha1@gmail.com>
2019-06-16 11:30:02 +0200

    simplified controller / routing logic

--- app.py
+++ app.py
@@ -9,33 +9,29 @@ shows_map = {show.name: show for show in shows}
 selected_show = shows[0]
 
 
-@app.route('/')
-def render():
-    # first time loading the show, have to scrap img and description
-    if not selected_show.img:
-        show_scraper.scrap_show_img_desc(selected_show, domain)
-
-    return render_template('show.html', shows=shows, selected=selected_show)
-
-
 @app.route('/', methods=['GET', 'POST'])
-def select_new_show():
-    global selected_show
-    selected_show = shows_map[request.form.get('show')]
-    return render()
+def render():
+    new_selected_show = request.form.get('show')
 
+    if new_selected_show:
+        global selected_show
+        selected_show = shows_map[request.form.get('show')]
 
-@app.route('/download', methods=['GET', 'POST'])
-def download_show():
     res = request.form.get('resolution')
     output_dir = request.form.get('output-directory')
-    if res.isdigit() and output_dir:
+
+    if res and res.isdigit() and output_dir:
         without_error, message = download_manager.download_show('magnet', selected_show.url,
                                                                 int(res), output_dir, selected_show.name)
         if not without_error:
             # should render another html file with error
             print(message)
-    return render()
+
+    # first time loading the show, have to scrap img and description
+    if not selected_show.img:
+        show_scraper.scrap_show_img_desc(selected_show, domain)
+
+    return render_template('show.html', shows=shows, selected=selected_show)
 
 
 if __name__ == '__main__':
